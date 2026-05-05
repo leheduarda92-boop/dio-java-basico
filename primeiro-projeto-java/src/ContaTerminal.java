6404e973a3e4a87e9749e5c0cc1840f71c803e1d
Leticia Carvalho <leheduarda92@gmail.com>
2026-05-05 12:00:56 -0300

    new file:   primeiro-projeto-java/.vscode/settings.json 	new file:   primeiro-projeto-java/README.md 	new file:   primeiro-projeto-java/src/App.java 	new file:   primeiro-projeto-java/src/ContaTerminal.java

---
+++ primeiro-projeto-java/src/ContaTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+ 
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor,digite o número da Agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero_conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o nome do cliente: ");
+        String nome_cliente = scanner.next();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero_conta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+}
